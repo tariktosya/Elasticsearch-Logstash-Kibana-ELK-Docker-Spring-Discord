895298391597f366f5785ddcc9da0244e5fd3bf8
tarik <tarik.tosya@gmail.com>
2023-03-26 14:58:18 +0300

    *

---
+++ src/main/java/com/demo/elk/elk/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.demo.elk.elk.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@Slf4j
+public class TestController {
+
+    @GetMapping("/elkTest")
+    public void elkTest() {
+        log.error("ERROR LOG TEST");
+        log.error("ERROR LOG TEST");
+        log.error("ERROR LOG TEST");
+        log.error("ERROR LOG TEST");
+        log.error("ERROR LOG TEST");
+        log.error("ERROR LOG TEST");
+        log.error("ERROR LOG TEST");
+    }
+}
